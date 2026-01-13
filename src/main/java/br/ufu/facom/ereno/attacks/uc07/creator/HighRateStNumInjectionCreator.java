7e4304268d592c8c04599bd95f366cf5f92fb760
bug-rankin <166861225+bug-rankin@users.noreply.github.com>
2026-01-13 09:02:55 -0500

    Refactor high-rate stNum injection to use burst flooding
    Updated the HighRateStNumInjectionCreator to generate flooding attacks in bursts, using random burst sizes and delays. Each burst is based on a random legitimate message, with stNum and sqNum incremented per message, and a short TTL set to simulate typical flooding attack behavior.

--- src/main/java/br/ufu/facom/ereno/attacks/uc07/creator/HighRateStNumInjectionCreator.java
+++ src/main/java/br/ufu/facom/ereno/attacks/uc07/creator/HighRateStNumInjectionCreator.java
@@ -21,28 +21,43 @@ public class HighRateStNumInjectionCreator implements MessageCreator {
     }
 
     /**
-     * Increases StNum at high rate
+     * Generates flooding attack with high-rate stNum increments in bursts
      */
     @Override
     public void generate(IED ied, int numberofMessages) {
-        double minGoose = legitimateMessages.get(0).getTimestamp();
-        double maxGoose = legitimateMessages.get(legitimateMessages.size()/2 - 1).getTimestamp();
-
-        double timestamp = randomBetween(minGoose, maxGoose);
-        double t = timestamp;
-        int stNum = 1;
-        int sqNum = 1;
-        int cbStatus = 1;
-
-        for (int i = 0; i < numberofMessages; i++) {
-            // Increases the stNum at a high rate (up to 100x the minTime)
-            double minTime = Double.valueOf(ConfigLoader.setupIED.minTime);
-            double randomDelay = randomBetween(minTime,minTime*2);
-            timestamp = timestamp + randomDelay;
-            stNum = stNum + 1;
-            // Send the generated message to InjectorIED
-            ied.addMessage(new Goose(cbStatus, stNum, sqNum, timestamp, t, GSVDatasetWriter.label[7]));
+        int messagesGenerated = 0;
+        double minTime = Double.valueOf(ConfigLoader.setupIED.minTime);
+        
+        while (messagesGenerated < numberofMessages) {
+            // Generate a burst of flooding messages
+            int burstSize = randomBetween(15, 200);
+            
+            // Pick a random legitimate message as a base
+            int idx = randomBetween(0, legitimateMessages.size() - 1);
+            Goose baseMessage = legitimateMessages.get(idx).copy();
+            
+            // Generate burst
+            for (int j = 0; j < burstSize && messagesGenerated < numberofMessages; j++) {
+                Goose g = baseMessage.copy();
+                g.setLabel(GSVDatasetWriter.label[7]);
+                
+                // Flooding: increment stNum on every packet
+                g.setStNum(baseMessage.getStNum() + j);
+                
+                // Increment sqNum with stride
+                int sqnumStride = randomBetween(1, 4);
+                g.setSqNum(baseMessage.getSqNum() + (j * sqnumStride));
+                
+                // Apply small random delay between messages in burst (0.05s to 0.5s)
+                double randomDelay = randomBetween((int)(minTime * 50), (int)(minTime * 500)) / 1000.0;
+                g.setTimestamp(baseMessage.getTimestamp() + (j * randomDelay));
+                
+                // Set short TTL typical of flooding attacks
+                g.setGooseTimeAllowedtoLive(20);
+                
+                ied.addMessage(g.copy());
+                messagesGenerated++;
+            }
         }
-
     }
 }
